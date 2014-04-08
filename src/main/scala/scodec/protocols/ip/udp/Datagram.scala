de133cd99da70d65a948d2c0186c44e59079ae45
Michael Pilquist <mpilquist@gmail.com>
2014-04-08 17:16:12 -0400

    Simplify udp example

--- src/main/scala/scodec/protocols/ip/udp/Datagram.scala
+++ src/main/scala/scodec/protocols/ip/udp/Datagram.scala
@@ -15,23 +15,16 @@ object Datagram {
   implicit val codec: Codec[Datagram] = {
     val port = Codec[Port]
 
-    val headerCodec =
-      ("source port"      | port) ::
-      ("destination port" | port) ::
-      ("length"           | uint16) ::
-      ("checksum"         | uint16)
-
     new Codec[Datagram] {
       def encode(dg: Datagram) = for {
-        encHeader <- headerCodec.encode(dg.sourcePort :: dg.destinationPort :: (8 + dg.data.bytes.size) :: 0 :: HNil)
+        encHeader <- Codec.encode(DatagramHeader(dg.sourcePort, dg.destinationPort, 8 + dg.data.bytes.size, 0))
         chksum = checksum(encHeader ++ dg.data)
       } yield encHeader.dropRight(16) ++ chksum ++ dg.data
 
       def decode(b: BitVector) = (for {
-        header <- DecodingContext(headerCodec.decode)
-        src :: dst :: length :: chksum :: HNil = header
-        data <- DecodingContext(bits(8 * (length - 8)).decode)
-      } yield Datagram(src, dst, data)).run(b)
+        header <- DecodingContext(Codec[DatagramHeader].decode)
+        data <- DecodingContext(bits(8 * (header.length - 8)).decode)
+      } yield Datagram(header.sourcePort, header.destinationPort, data)).run(b)
     }
 
   }
