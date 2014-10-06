7f3223faa7b16286c114260a7c55a3da32f3318a
Michael Pilquist <mpilquist@gmail.com>
2014-10-06 19:20:39 -0400

    Cleanup section grouping and support custom grouping for non-extended sections

--- src/test/scala/scodec/protocols/mpeg/transport/psi/GroupingTest.scala
+++ src/test/scala/scodec/protocols/mpeg/transport/psi/GroupingTest.scala
@@ -13,7 +13,7 @@ class GroupingTest extends ProtocolsSpec {
 
     "support grouping a stream of extended sections in to a stream of grouped sections" which {
 
-      val des = desectionExtendedSections[ProgramAssociationSection]
+      val des = gropuExtendedSections[ProgramAssociationSection]
 
       val pat3: ProgramAssociationTable = {
         val pidMap = (0 until ProgramAssociationTable.MaxProgramsPerSection * 3).map { n => ProgramNumber(n) -> Pid(n) }.toMap
