6ef007ea2ad7abcc07d2672a3247818c2acda9d2
Michael Pilquist <mpilquist@gmail.com>
2014-12-01 13:37:45 -0500

    Fix bug in ipv4 SimpleHeader

--- src/main/scala/scodec/protocols/ip/v4/SimpleHeader.scala
+++ src/main/scala/scodec/protocols/ip/v4/SimpleHeader.scala
@@ -22,30 +22,39 @@ case class SimpleHeader(
 object SimpleHeader {
 
   implicit val codec: Codec[SimpleHeader] = {
-    val componentCodec =
-      ("version" | bin"0100") :~>:
-      ("ihl" | uint8) ::
-      ("total_length" | uint16) ::
-      ("id" | uint16) ::
-      ignore(16) ::
-      ("ttl" | uint8) ::
-      ("proto" | uint8) ::
-      ("checksum" | bits(16)) ::
-      ("src_ip" | Codec[Address]) ::
-      ("dest_ip" | Codec[Address])
+    val componentCodec = {
+      // Word 1 --------------------------------
+      ("version"         | bin"0100"     ) ::
+      ("ihl"             | uint4         ) ::
+      ("dscp"            | ignore(6)     ) ::
+      ("ecn"             | ignore(2)     ) ::
+      ("total_length"    | uint16        ) ::
+      // Word 2 --------------------------------
+      ("id"              | uint16        ) ::
+      ("flags"           | ignore(3)     ) ::
+      ("fragment_offset" | ignore(13)    ) ::
+      // Word 3 --------------------------------
+      ("ttl"             | uint8         ) ::
+      ("proto"           | uint8         ) ::
+      ("checksum"        | bits(16)      ) ::
+      // Word 4 --------------------------------
+      ("src_ip"          | Codec[Address]) ::
+      // Word 5 --------------------------------
+      ("dest_ip"         | Codec[Address])
+    }.dropUnits
 
     new Codec[SimpleHeader] {
       def encode(header: SimpleHeader) = {
         val totalLength = header.dataLength + 20
         for {
-          encoded <- componentCodec.encode(5 :: totalLength :: header.id :: () :: header.ttl :: header.protocol :: BitVector.low(16) :: header.sourceIp :: header.destinationIp :: HNil)
+          encoded <- componentCodec.encode(5 :: totalLength :: header.id :: header.ttl :: header.protocol :: BitVector.low(16) :: header.sourceIp :: header.destinationIp :: HNil)
           chksum = checksum(encoded)
         } yield encoded.patch(16 + 16 + 16, chksum)
       }
 
       def decode(bits: BitVector) = {
         componentCodec.decode(bits) map {
-          case (rest, _ :: totalLength :: id :: _ :: ttl :: proto :: chksum :: srcIp :: dstIp :: HNil) =>
+          case (rest, _ :: totalLength :: id :: ttl :: proto :: chksum :: srcIp :: dstIp :: HNil) =>
             rest -> SimpleHeader(totalLength - 20, id, ttl, proto, srcIp, dstIp)
         }
       }
