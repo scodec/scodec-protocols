6ec2d6466b74d4e48fea66576fec54c818c42fa7
Michael Pilquist <mpilquist@gmail.com>
2014-12-10 13:40:29 -0500

    Add the ability to create a TransportStreamEvent stream from a section stream

--- src/main/scala/scodec/protocols/mpeg/transport/psi/TransportStreamEvent.scala
+++ src/main/scala/scodec/protocols/mpeg/transport/psi/TransportStreamEvent.scala
@@ -4,6 +4,7 @@ package transport
 package psi
 
 import scalaz.{ \/, \/-, -\/ }
+import \/.right
 import scalaz.stream._
 
 import psi.{ Table => TableMessage }
@@ -20,41 +21,49 @@ object TransportStreamEvent {
   def metadata[A](pid: Pid, md: A): TransportStreamEvent = Metadata(Some(pid), md)
   def error(pid: Pid, e: MpegError): TransportStreamEvent = Error(pid, e)
 
-  def stream(
-    sectionCodec: SectionCodec,
-    tableBuilder: TableBuilder,
-    group: Process1[Section, GroupingError \/ GroupedSections] = GroupedSections.group
-  ): Process1[Packet, TransportStreamEvent] = {
-
+  private def sectionsToTables(
+    group: Process1[Section, GroupingError \/ GroupedSections], tableBuilder: TableBuilder
+  ): Process1[PidStamped[MpegError \/ Section], TransportStreamEvent] = {
+    type SectionsToTables = Process1[PidStamped[MpegError \/ Section], PidStamped[MpegError \/ TableMessage]]
+    type P = Process1[PidStamped[MpegError \/ Section], PidStamped[MpegError \/ (TransportStreamIndex \/ TableMessage)]]
     import MpegError._
 
-    val sectionsToTables: Process1[PidStamped[MpegError \/ Section], PidStamped[MpegError \/ (TransportStreamIndex \/ TableMessage)]] = {
+    def newSectionsToTablesForPid: SectionsToTables =
+      PidStamped.preservePidStamps(joinErrors(group) |> joinErrors(tableBuilder.sectionsToTables))
 
-      type SectionsToTables = Process1[PidStamped[MpegError \/ Section], PidStamped[MpegError \/ TableMessage]]
-      type P = Process1[PidStamped[MpegError \/ Section], PidStamped[MpegError \/ (TransportStreamIndex \/ TableMessage)]]
-
-      def newSectionsToTablesForPid: SectionsToTables =
-        PidStamped.preservePidStamps(joinErrors(group) |> joinErrors(tableBuilder.sectionsToTables))
-
-      def go(state: Map[Pid, SectionsToTables]): SectionsToTables = {
-        def cleanup = state.values.foldLeft(Process.halt: SectionsToTables) { (acc, p) => acc ++ p.disconnect(Cause.Kill) }
-        Process.receive1Or(cleanup) {
-          case event =>
-            val p = state.getOrElse(event.pid, newSectionsToTablesForPid)
-            val (toEmit, next) = p.feed1(event).unemit
-            Process.emitAll(toEmit) ++ go(state + (event.pid -> next))
-        }
+    def go(state: Map[Pid, SectionsToTables]): SectionsToTables = {
+      def cleanup = state.values.foldLeft(Process.halt: SectionsToTables) { (acc, p) => acc ++ p.disconnect(Cause.Kill) }
+      Process.receive1Or(cleanup) {
+        case event =>
+          val p = state.getOrElse(event.pid, newSectionsToTablesForPid)
+          val (toEmit, next) = p.feed1(event).unemit
+          Process.emitAll(toEmit) ++ go(state + (event.pid -> next))
       }
-
-      go(Map.empty) |> PidStamped.preservePidStamps(passErrors(TransportStreamIndex.build))
     }
 
-    sectionCodec.depacketize.pipe(sectionsToTables).map {
+    val result = go(Map.empty) pipe PidStamped.preservePidStamps(passErrors(TransportStreamIndex.build))
+
+    result map {
       case PidStamped(pid, value) => value match {
         case -\/(e) => error(pid, e)
         case \/-(-\/(tsi)) => metadata(tsi)
         case \/-(\/-(tbl)) => table(pid, tbl)
       }
     }
+  }
+
+  def fromPacketStream(
+    sectionCodec: SectionCodec,
+    tableBuilder: TableBuilder,
+    group: Process1[Section, GroupingError \/ GroupedSections] = GroupedSections.group
+  ): Process1[Packet, TransportStreamEvent] = {
+    sectionCodec.depacketize.pipe(sectionsToTables(group, tableBuilder))
+  }
+
+  def fromSectionStream(
+    tableBuilder: TableBuilder,
+    group: Process1[Section, GroupingError \/ GroupedSections] = GroupedSections.group
+  ): Process1[PidStamped[Section], TransportStreamEvent] = {
+    process1.lift((p: PidStamped[Section]) => p map right[MpegError, Section]) pipe sectionsToTables(group, tableBuilder)
   }
 }
