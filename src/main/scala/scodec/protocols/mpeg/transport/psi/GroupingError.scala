22d7575d200bc66e5a6cf2d3cbdb76672e8bb9d0
Michael Pilquist <mpilquist@gmail.com>
2015-10-30 14:30:26 -0400

    Make table extension optional in GroupingError

--- src/main/scala/scodec/protocols/mpeg/transport/psi/GroupingError.scala
+++ src/main/scala/scodec/protocols/mpeg/transport/psi/GroupingError.scala
@@ -2,4 +2,12 @@ package scodec.protocols.mpeg
 package transport
 package psi
 
-case class GroupingError(tableId: Int, tableIdExtension: Int, message: String) extends MpegError
+case class GroupingError(tableId: Int, tableIdExtension: Option[Int], message: String) extends MpegError
+
+object GroupingError {
+  def apply(tableId: Int, tableIdExtension: Int, message: String): GroupingError =
+    new GroupingError(tableId, Some(tableIdExtension), message)
+
+  def apply(tableId: Int, message: String): GroupingError =
+    new GroupingError(tableId, None, message)
+}
