d7c27ba6c562260348baa82505ecde6dfaae5c18
Michael Pilquist <mpilquist@gmail.com>
2020-05-02 16:12:08 -0400

    Changed mpeg pcr/opcr from BitVector to Clock27MHz

--- src/main/scala/scodec/protocols/mpeg/transport/AdaptationField.scala
+++ src/main/scala/scodec/protocols/mpeg/transport/AdaptationField.scala
@@ -41,8 +41,8 @@ import scodec.codecs._
  */
 case class AdaptationField(
   flags: Option[AdaptationFieldFlags],
-  pcr: Option[BitVector],
-  opcr: Option[BitVector],
+  pcr: Option[Clock27MHz],
+  opcr: Option[Clock27MHz],
   spliceCountdown: Option[Int],
   transportPrivateData: Option[BitVector]
 )
@@ -54,18 +54,27 @@ object AdaptationField {
   implicit val codec: Codec[AdaptationField] = new Codec[AdaptationField] {
     private case class NonEmptyAF(
       flags: AdaptationFieldFlags,
-      pcr: Option[BitVector],
-      opcr: Option[BitVector],
+      pcr: Option[Clock27MHz],
+      opcr: Option[Clock27MHz],
       spliceCountdown: Option[Int],
       transportPrivateData: Option[BitVector]
     ) { def asAF: AdaptationField = AdaptationField(Some(flags), pcr, opcr, spliceCountdown, transportPrivateData) }
 
+    private val pcrCodec: Codec[Clock27MHz] =
+      ((ulong(33) <~ ignore(6)) ~ uint(9)).xmapc { case (base, ext) =>
+        Clock27MHz(base * 300 + ext)
+      } { clock =>
+        val value = clock.value
+        val base = value / 300
+        val ext = (value % 300).toInt
+        (base, ext)
+      }
     private val transportPrivateData: Codec[BitVector] = variableSizeBits(uint8, bits)
     private val nonEmptyAFCodec: Codec[NonEmptyAF] = "adaptation_field" | {
       variableSizeBytes(uint8,
         ("adaptation_flags"       | Codec[AdaptationFieldFlags]                ).flatPrepend { flags =>
-        ("pcr"                    | conditional(flags.pcrFlag, bits(48))       ) ::
-        ("opcr"                   | conditional(flags.opcrFlag, bits(48))      ) ::
+        ("pcr"                    | conditional(flags.pcrFlag, pcrCodec)       ) ::
+        ("opcr"                   | conditional(flags.opcrFlag, pcrCodec)      ) ::
         ("splice_countdown"       | conditional(flags.splicingPointFlag, int8) ) ::
         ("transport_private_data" | conditional(flags.transportPrivateDataFlag, transportPrivateData))
       })
