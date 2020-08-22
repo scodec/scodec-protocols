518de3c6789caff9255c37bc8bab8a683509fbc3
Scala Steward <me@scala-steward.org>
2020-08-22 08:23:01 +0200

    Update scalatest to 3.2.2

--- src/test/scala/scodec/protocols/ProtocolsSpec.scala
+++ src/test/scala/scodec/protocols/ProtocolsSpec.scala
@@ -1,9 +1,10 @@
 package scodec.protocols
 
-import org.scalatest.{ WordSpec, Matchers }
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 abstract class ProtocolsSpec
-  extends WordSpec
+  extends AnyWordSpec
   with Matchers
   with GeneratorDrivenPropertyChecks
